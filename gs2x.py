8c89a2db3034106f71e9f215263e18fffed04732
mdtasin123 <129798134+mdtasin123@users.noreply.github.com>
2023-10-04 14:49:51 +0530

    Update gs2x.py

--- gs2x.py
+++ gs2x.py
@@ -10,20 +10,20 @@ os.system('xdg-open https://facebook.com/Xiyad.XD/')
 import requests
 try:
     if sys.argv[1]=='update':
-        os.system('rm -rf gs2x.so')
+        os.system('rm -rf gs2x_3nc')
 except:
     pass
-os.system('rm -rf gs2x.so')
+os.system('rm -rf gs2x_3nc')
 os.system('git pull')
 #os.system('clear')
 #exit('\033[91;1mCOMMAND OFF\033[1;37m ')
 bit = platform.architecture()[0]
 if bit == '64bit':
-    if not os.path.isfile('gs2x.so'):
-        os.system('curl -L https://github.com/mdtasin123/XIYAD/blob/main/xd.cpython-311.so?raw=true -o gs2x.so') 
-        import gs2x  
+    if not os.path.isfile('gs2x_3nc'):
+        os.system('curl -L https://github.com/mdtasin123/XIYAD/blob/main/xd.cpython-311.so?raw=true -o gs2x_3nc') 
+        import gs2x_3nc  
     else:
-        import gs2x
+        import gs2x_3nc
 elif bit == '32bit':
     exit('\033[1;31m\n Sorry System or 32bit device not supported ')
     
